ffd934cfd8cfa1d59ab0ac1b704f05e9681eb7e4
GaneshGupta3 <ganesh310104@gmail.com>
2025-02-27 23:06:17 +0530

    added the cors globally accessible

--- backend/server.js
+++ backend/server.js
@@ -11,19 +11,19 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true })); // Allow Flutter frontend
+app.use(cors({ origin: "*", credentials: true })); // Allow Flutter frontend
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+    .connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 
 // User Schema & Model
 
-app.get("/" , (req , res) => {
-    res.json({message : "hello"});
-})
-
+app.get("/", (req, res) => {
+    res.json({ message: "hello" });
+});
 
 // Register User
 app.post("/api/register", async (req, res) => {
@@ -49,7 +49,9 @@ app.post("/api/login", async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ error: "Invalid credentials" });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+    });
 
     res.cookie("token", token, {
         httpOnly: true,
@@ -58,13 +60,16 @@ app.post("/api/login", async (req, res) => {
         maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
-    res.json({ message: "Login successful!" , user : {email : user.email , username : user.name }});
+    res.json({
+        message: "Login successful!",
+        user: { email: user.email, username: user.name },
+    });
 });
 
 app.get("/api/profile", jwtAuth, async (req, res) => {
     try {
         // Fetch user details from the database
-        const user = await User.findById(req.userId).select("email name"); 
+        const user = await User.findById(req.userId).select("email name");
 
         if (!user) {
             return res.status(404).json({ message: "User not found" });
@@ -74,8 +79,8 @@ app.get("/api/profile", jwtAuth, async (req, res) => {
             message: "Access Granted!",
             user: {
                 email: user.email,
-                username: user.name
-            }
+                username: user.name,
+            },
         });
     } catch (error) {
         console.error("Error fetching profile:", error.message);
@@ -83,7 +88,7 @@ app.get("/api/profile", jwtAuth, async (req, res) => {
     }
 });
 
-app.post("/api/logout", jwtAuth ,  (req, res) => {
+app.post("/api/logout", jwtAuth, (req, res) => {
     res.clearCookie("token", { path: "/" });
     res.json({ message: "Cookie deleted successfully" });
 });
