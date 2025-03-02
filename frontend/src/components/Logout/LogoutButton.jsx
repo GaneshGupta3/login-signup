da49744718464a5938c9a80c37f4baf2232468c5
GaneshGupta3 <ganesh310104@gmail.com>
2025-03-02 21:29:29 +0530

    whole login system is finally implemented successfully

--- frontend/src/components/Logout/LogoutButton.jsx
+++ frontend/src/components/Logout/LogoutButton.jsx
@@ -1,18 +1,9 @@
 import React from "react";
 import { IoIosLogOut } from "react-icons/io";
-import { useDispatch } from "react-redux";
-import { authSliceActions, logoutAsync } from "../../store/authSlice";
-import { useNavigate } from "react-router-dom";
 
 const LogoutButton = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        dispatch(logoutAsync()); // Calls API & updates Redux state
-        navigate("/login");
-    };
 
-    return <IoIosLogOut style={{cursor : "pointer"}} onClick={handleLogout} size={40} />;
+    return <IoIosLogOut style={{cursor : "pointer"}} size={40} />;
 };
 
 export default LogoutButton;
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,34 +1,56 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import StyledButton from "../StyledButton/StyledButton";
 import LogoutButton from "../Logout/LogoutButton";
+import { logoutAsync } from "../../store/authSlice";
 
 const Navbar = ({ transparent }) => {
-
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const { isLoggedIn } = useSelector((store) => store.authProvider);
 
-    const loginRedirect = ()=>{
+    const loginRedirect = () => {
         navigate("/login");
-    }
-    const signupRedirect = ()=>{
+    };
+    const signupRedirect = () => {
         navigate("/signup");
-    }
+    };
+
+    const handleLogout = () => {
+        dispatch(logoutAsync()); // Calls API & updates Redux state
+        navigate("/login");
+    };
 
     return (
         <nav className={`${styles.navbar} ${transparent ? styles.transparent : ""}`}>
             <div className={styles.logo}></div>
+            <h1>Dashboard</h1>
             <h1>About Us</h1>
             <h1>Problems</h1>
             <h1>Rankings</h1>
             <h1>Contact Us</h1>
-            {!isLoggedIn && <div>
-                <StyledButton executeFunction={loginRedirect} displayText={"Login"}></StyledButton>
-                <StyledButton executeFunction={signupRedirect} displayText={"Signup"}>Signup</StyledButton>
-            </div>}
-            {isLoggedIn && <LogoutButton />}
+            {!isLoggedIn && (
+                <div>
+                    <StyledButton
+                        executeFunction={loginRedirect}
+                        displayText={"Login"}
+                    ></StyledButton>
+                    <StyledButton
+                        executeFunction={signupRedirect}
+                        displayText={"Signup"}
+                    >
+                        Signup
+                    </StyledButton>
+                </div>
+            )}
+            {isLoggedIn && (
+                <div className={styles.logoutDiv} onClick={handleLogout}>
+                    Logout
+                    <LogoutButton />
+                </div>
+            )}
         </nav>
     );
 };
